876ecd423632f414c59195ee6eedd59f51249fb9
EC2 Default User <ec2-user@ip-172-31-26-230.ec2.internal>
2020-02-18 00:25:21 +0000

    adding checkmarks

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
+-#    @@all_ratings = ['G', 'PG', 'PG-13', 'R']
+-#    def self.all_ratings
+-#        @@all_ratings
 class Movie < ActiveRecord::Base
-    @@all_ratings = ['G', 'PG', 'PG-13', 'R']
-    def self.all_ratings
-        @@all_ratings
-    end
 end
